fe0068944c63f2bb2a1d03c1842418df6833f1ae
sava17 <37406543+sava17@users.noreply.github.com>
2018-04-27 11:19:20 +0200

    Block klassen og tilføjede blackboxtest mappe

---
+++ BlackBoxTests/BlockTest.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test;
+
+public class BlockTest {
+    @Test
+    void getTimestamp() {
+        Block testBlock = new Block();
+        System.out.println(testBlock.getTimestamp());
+    }
+}
