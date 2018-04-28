9d4c229ae2b5a5550b3764a2b95e4998b0a6d8b4
Seraph2 <frostbyte9@hotmail.com>
2018-04-28 23:10:24 +0200

    Revert "Block klassen og tilføjede blackboxtest mappe"
    This reverts commit fe0068944c63f2bb2a1d03c1842418df6833f1ae.

--- BlackBoxTests/BlockTest.java
+++
@@ -1,9 +0,0 @@
-import org.junit.jupiter.api.Test;
-
-public class BlockTest {
-    @Test
-    void getTimestamp() {
-        Block testBlock = new Block();
-        System.out.println(testBlock.getTimestamp());
-    }
-}
